d911e8bb12cc97f8787fb11ebf4003ecc2a77ca6
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-01-29 16:29:10 +0530

    [pages]: add basic auth pages

---
+++ lib/pages/auth/login_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  build(context) => Container();
+}
---
+++ lib/pages/auth/register_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  build(context) => Container();
+}
