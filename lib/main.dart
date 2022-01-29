5b785b7235d5ee3c21b6d4db73e1a59c065d2670
Sudhindra3000 <55095527+Sudhindra3000@users.noreply.github.com>
2022-01-29 16:39:41 +0530

    call await Firebase.initializeApp() in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:bug_tracker/pages/auth/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   build(context) {
     return MaterialApp(
